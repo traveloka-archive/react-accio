3f7f180dbf1308f806c5e382a52280135994e673
Raibima Putra <raibima.putra@traveloka.com>
2018-08-12 02:23:51 +0700

    Default resolver improvement

--- src/defaults/resolver.js
+++ src/defaults/resolver.js
@@ -6,6 +6,11 @@ export default async function resolver(url, fetchOptions) {
   if (err) {
     throw new Error('Accio error: ' + err.message);
   }
+  if (res.ok === false) {
+    throw new Error(
+      `Accio failed to fetch: ${res.url} ${res.status} (${res.statusText})`
+    );
+  }
   const [err2, jsonResponse] = await to(res.json());
   if (err2) {
     throw new Error('Error parsing response to json: ' + err2.message);
